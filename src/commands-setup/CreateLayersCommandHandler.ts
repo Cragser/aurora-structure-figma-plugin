4b27a2c807098f62554f423a0baa1a47eaf52a84
Sergio Reyes <sergio.reyes@plexusworldwide.com>
2024-02-01 00:24:26 -0600

    2. Crear primeras instrucciones para manegar páginas

--- src/commands-setup/CreateLayersCommandHandler.ts
+++ src/commands-setup/CreateLayersCommandHandler.ts
@@ -1,13 +1,34 @@
-import { CreateLayersCommand } from "./CreateLayersCommand";
 import { CommandHandler } from "./CommandHandler";
+import { CreateLayersCommand } from "./CreateLayersCommand";
 
-export class CreateLayersCommandHandler implements CommandHandler<CreateLayersCommand>
+export class CreateLayersCommandHandler
+  implements CommandHandler<CreateLayersCommand>
 {
   constructor(private readonly figma: PluginAPI) {}
 
   handle(command: CreateLayersCommand): Promise<void> | void {
-    const message = 'Hello from CreateLayersCommandHandler! 🎉';
+    this.createPages();
+    this.notifySuccess();
+  }
+
+  private notifySuccess() {
+    const message = "Layers created successfully!";
     const options = { timeout: 5000 };
     this.figma.notify(message, options);
+  }
+
+  private createPages() {
+    this.renameCurrentPageToCover();
+    const pages = ["---", "Desktop", "Tablet", "Mobile", "---", "Graveyard"];
+    pages.forEach((name) => this.createPage(name));
+  }
+
+  private renameCurrentPageToCover() {
+    this.figma.currentPage.name = "Cover";
+  }
+
+  private createPage(name: string) {
+    const page = this.figma.createPage();
+    page.name = name;
   }
 }
